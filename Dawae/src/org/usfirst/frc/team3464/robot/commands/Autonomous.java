9a6035b9523d537f245e7b358ae7df0a302e9ca6
Asuka Langley <ap@tfwno.gf>
2018-01-17 02:42:50 -0500

    rm -r //

--- Dawae/src/org/usfirst/frc/team3464/robot/commands/Autonomous.java
+++ Dawae/src/org/usfirst/frc/team3464/robot/commands/Autonomous.java
@@ -34,24 +34,19 @@ public class Autonomous extends CommandGroup {
 		gsm = Robot.ds.getGameSpecificMessage();
 	}
 
-	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
 	}
 
-	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
 		return false;
 	}
 
-	// Called once after isFinished returns true
 	@Override
 	protected void end() {
 	}
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
 	@Override
 	protected void interrupted() {
 	}
