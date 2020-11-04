f41c5e292047b7f4b60047676eefbf802a9b9b5f
Aaron Paterson <apaterson@protonmail.com>
2020-11-04 09:07:10 -0500

    Update DriveLine.java

--- Dawae/src/org/usfirst/frc/team3464/robot/subsystems/DriveLine.java
+++ Dawae/src/org/usfirst/frc/team3464/robot/subsystems/DriveLine.java
@@ -7,9 +7,8 @@ import org.usfirst.frc.team3464.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class DriveLine extends Subsystem {
-
-	public double zRotation(double angle) {
+public class DriveLine extends Subsystem { // Controls robot movement
+	public double zRotation(double angle) { // Utility function to find shortest path between two between angles
 		double
 			rad = (angle-RobotMap.gyro.getAngle()) * Math.PI / 180.0,
 			radnorm = 2.0 / Math.PI,
@@ -20,15 +19,15 @@ public class DriveLine extends Subsystem {
 			// Math.atan( 1.0 / Math.tan( rad/2.0 ) ) * radnorm * defence;
 	}
 
-	private double idRange(double n) {
+	private double idRange(double n) { // Utility function to clamp inputs between -1,1
 		return n > 1.0 ? 1.0 : n < -1.0 ? -1.0 : n;
 	}
 
-	public void arcade(double y, double z) {
+	public void arcade(double y, double z) { // Arcade controls, implemented as tank controls
 		RobotMap.drive.tankDrive(idRange( y + z ), idRange( y - z ));
 	}
 
-	public Consumer<Void> teleTank = (Void) -> {
+	public Consumer<Void> teleTank = (Void) -> { // Tank drive
 		/** TOKYO DRIFT
 		double
 			l = Robot.operatorInterface.leftStick.getTrigger() ? 0.0 : Robot.operatorInterface.leftStick.getY(),
@@ -46,7 +45,7 @@ public class DriveLine extends Subsystem {
 		RobotMap.drive.tankDrive(l*Math.abs(l), r*Math.abs(r));
 	};
 
-	public Consumer<Void> teleTank180 = (Void) -> {
+	public Consumer<Void> teleTank180 = (Void) -> { // Reverse tank drive for backing up
 		double ls =.6*Robot.operatorInterface.leftStick.getY(), rs = -.6*Robot.operatorInterface.rightStick.getY();
 		double l = ls, r = rs;
 		
@@ -56,7 +55,7 @@ public class DriveLine extends Subsystem {
 		RobotMap.drive.tankDrive(r*Math.abs(r), l*Math.abs(l));
 	};
 
-	public Consumer<Void> teleArcade = (Void) -> {
+	public Consumer<Void> teleArcade = (Void) -> { // Arcade drive
 		double
 			y = Robot.operatorInterface.leftStick.getY(),
 			z = Robot.operatorInterface.leftStick.getZ(),
