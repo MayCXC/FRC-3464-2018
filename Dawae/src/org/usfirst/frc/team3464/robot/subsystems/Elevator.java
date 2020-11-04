0496ab13055b44d2ac33d0b1aac1ea2d2d8c3251
Aaron Paterson <apaterson@protonmail.com>
2020-11-04 09:07:43 -0500

    Update Elevator.java

--- Dawae/src/org/usfirst/frc/team3464/robot/subsystems/Elevator.java
+++ Dawae/src/org/usfirst/frc/team3464/robot/subsystems/Elevator.java
@@ -4,7 +4,7 @@ import org.usfirst.frc.team3464.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Elevator extends Subsystem {
+public class Elevator extends Subsystem { // Check limit switches and move big lift
 
 	public void set(double lift) {
 		if ( (RobotMap.elevatorLow.get() && lift < 0.0) || (RobotMap.elevatorHigh.get() && lift > 0.0) )
