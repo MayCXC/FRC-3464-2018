8c79d78d5d7bad3e099af3e47af2efc3e73c86dd
Aaron Paterson <apaterson@protonmail.com>
2020-11-04 08:53:53 -0500

    Update Robot.java

--- Dawae/src/org/usfirst/frc/team3464/robot/Robot.java
+++ Dawae/src/org/usfirst/frc/team3464/robot/Robot.java
@@ -12,22 +12,22 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-	public static final DriveLine driveLine = new DriveLine();
-	public static final Elevator elevator = new Elevator();
-	public static final Claw claw = new Claw();
+	public static final DriveLine driveLine = new DriveLine(); // Motor subsystem
+	public static final Elevator elevator = new Elevator(); // Lift subsystem
+	public static final Claw claw = new Claw(); // Arm subsystem
 
-	public static OperatorInterface operatorInterface;
+	public static OperatorInterface operatorInterface; // Takes input from joysticks and GUI
 
-	SendableChooser<Autonomous> autoChooser;
+	SendableChooser<Autonomous> autoChooser; // GUI selector to pick which autonomous preset to attempt
 	Command autoCommand = null;
 
-	SendableChooser<Teleoperated> teleChooser;
+	SendableChooser<Teleoperated> teleChooser; // GUI selector to pick between tank and arcade controls
 	Command teleCommand = null;
 	
-	SendableChooser<Boolean> killCompressor;
+	SendableChooser<Boolean> killCompressor; // GUI selector to turn compressor off
 
 	@Override
-	public void robotInit() {
+	public void robotInit() { // Instantiate subsystems and connect to GUI
 		operatorInterface = new OperatorInterface();
 
 		autoChooser = new SendableChooser<>();
@@ -50,11 +50,11 @@ public class Robot extends TimedRobot {
 		killCompressor.addObject("Compressor off", false);
 		SmartDashboard.putData("Kill Compressor", killCompressor);
 
-		new Thread( CameraServer.getInstance()::startAutomaticCapture ).start();
+		new Thread( CameraServer.getInstance()::startAutomaticCapture ).start(); // View webcam without blocking subsystems
 	}
 
 	@Override
-	public void robotPeriodic() {
+	public void robotPeriodic() { // Update GUI as frequently as possible
 		SmartDashboard.putBoolean("WINCH LOW", RobotMap.elevatorLow.get());
 		SmartDashboard.putBoolean("WINCH HIGH", RobotMap.elevatorHigh.get());
 		SmartDashboard.putNumber("LEFT ENCODER", RobotMap.leftEncoder.getDistance());
@@ -73,30 +73,30 @@ public class Robot extends TimedRobot {
 	}
 
 	@Override
-	public void disabledPeriodic() {
+	public void disabledPeriodic() { // Close arm before match
 		Scheduler.getInstance().run();
 		Scheduler.getInstance().add(new Grab(true));
 	}
 
 	@Override
-	public void autonomousInit() {
+	public void autonomousInit() { // Wait until last second to read which auto to use
 		autoCommand = autoChooser.getSelected();
 		if (autoCommand != null) autoCommand.start();
 	}
 
 	@Override
-	public void autonomousPeriodic() {
+	public void autonomousPeriodic() { // Pass control to subsystems
 		Scheduler.getInstance().run();
 	}
 
 	@Override
-	public void teleopInit() {
+	public void teleopInit() { // Stop auto and start tele
 		if (autoCommand != null) autoCommand.cancel();
 		Scheduler.getInstance().removeAll();
 	}
 
 	@Override
-	public void teleopPeriodic() {
+	public void teleopPeriodic() { // Pick control scheme from OI and check compressor switch
 		Scheduler.getInstance().run();
 		teleCommand = teleChooser.getSelected();
 		if (teleCommand != null) {
