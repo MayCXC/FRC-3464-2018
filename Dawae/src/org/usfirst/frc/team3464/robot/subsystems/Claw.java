f49a0e64f4edfe11aa01a35f4933f7a19457b7ed
Aaron Paterson <apaterson@protonmail.com>
2020-11-04 09:02:53 -0500

    Update Claw.java

--- Dawae/src/org/usfirst/frc/team3464/robot/subsystems/Claw.java
+++ Dawae/src/org/usfirst/frc/team3464/robot/subsystems/Claw.java
@@ -2,11 +2,11 @@ package org.usfirst.frc.team3464.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Claw extends Subsystem {
+public class Claw extends Subsystem { // Literally just toggle the claw state
 	public boolean grabState = false;
 	
 	@Override
-    public void initDefaultCommand() {
+	public void initDefaultCommand() {
 	}
 }
 
